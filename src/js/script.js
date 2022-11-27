0e52753f33297b00d680794684a89d2c44cf653d
Krivoruchko Maxim <23maxikriv07@gmail.com>
2022-11-27 23:36:57 +0500

    add script for open/close modal

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+    const btns = document.querySelectorAll('.btn'),
+          modal = document.querySelector('.modal'),
+          closeModalBtn = document.querySelector('.modal_close');
+
+    const openModal = () => {
+        btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                modal.style.display = 'block';
+            });
+        });
+    };
+    openModal();
+    
+    const closeModal = () => {
+        closeModalBtn.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target == modal) {
+                modal.style.display = 'none';
+            }
+            console.log(e.target);
+        });
+        
+    };
+    closeModal();
+});
